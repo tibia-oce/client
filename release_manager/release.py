d5f1aa026fcb903bdd2a7f706b8fe9dabf667585
Jordan Hoare <jordanhoare0@gmail.com>
2024-10-14 10:07:08 +0800

    migrate pipeline code to module

--- release_manager/release.py
+++ release_manager/release.py
@@ -1,11 +1,13 @@
-import os
-import json
-import requests
 import hashlib
+import json
+import os
 import tarfile
 import zipfile
 from dataclasses import dataclass, field
-from typing import List, Dict, Union
+from typing import Any
+
+import requests
+
 
 @dataclass
 class FileInfo:
@@ -14,63 +16,78 @@ class FileInfo:
     size: int
     url: str
 
+
 @dataclass
 class ModulesJSON:
-    version: Union[str, None] = "1"
-    files: List[FileInfo] = field(default_factory=list)
+    version: str | None = "1"
+    files: list[FileInfo] = field(default_factory=list)
+
 
 @dataclass
 class DataJSON:
-    version: Union[str, None] = "1"
-    files: List[FileInfo] = field(default_factory=list)
+    version: str | None = "1"
+    files: list[FileInfo] = field(default_factory=list)
+
 
 @dataclass
 class ModsJSON:
-    version: Union[str, None] = "1"
-    files: List[FileInfo] = field(default_factory=list)
+    version: str | None = "1"
+    files: list[FileInfo] = field(default_factory=list)
+
 
 @dataclass
 class ClientJSON:
-    version: Union[str, None] = "1"
+    version: str | None = "1"
     revision: int = 1
-    executable: Union[str, None] = "otclient.exe"
-    files: List[FileInfo] = field(default_factory=list)
+    executable: str | None = "otclient.exe"
+    files: list[FileInfo] = field(default_factory=list)
+
 
 @dataclass
 class ReleaseAsset:
     name: str
     browser_download_url: str
 
-def fetch_release_data(repo: str, tag_name: str) -> dict:
+
+def fetch_release_data(repo: str, tag_name: str) -> Any:
+    # todo: create a data class for this response
     url = f"https://api.github.com/repos/{repo}/releases/tags/{tag_name}"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
-def get_release_asset(release_data: dict) -> ReleaseAsset:
-    asset_data = release_data['assets'][0]
-    return ReleaseAsset(name=asset_data['name'], browser_download_url=asset_data['browser_download_url'])
 
-def download_asset(asset: ReleaseAsset, download_dir: str = "/tmp") -> str:
+def get_release_asset(release_data: dict[str, Any]) -> ReleaseAsset:
+    asset_data = release_data["assets"][0]
+    return ReleaseAsset(
+        name=asset_data["name"], browser_download_url=asset_data["browser_download_url"]
+    )
+
+
+def download_asset(asset: ReleaseAsset, download_dir: str = "./windows") -> str:
+    os.makedirs(download_dir, exist_ok=True)
     response = requests.get(asset.browser_download_url, stream=True)
     response.raise_for_status()
     asset_path = os.path.join(download_dir, asset.name)
-    with open(asset_path, 'wb') as f:
+    with open(asset_path, "wb") as f:
         for chunk in response.iter_content(chunk_size=8192):
             f.write(chunk)
-    print(f"{asset.name} downloaded successfully.")
+    print(f"{asset.name} downloaded successfully to {asset_path}.")
     return asset_path
 
+
 def extract_archive(archive_path: str, extract_to: str) -> None:
-    if archive_path.endswith('.zip'):
-        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+    os.makedirs(extract_to, exist_ok=True)
+    if archive_path.endswith(".zip"):
+        with zipfile.ZipFile(archive_path, "r") as zip_ref:
             zip_ref.extractall(extract_to)
-    elif archive_path.endswith(('.tar.gz', '.tgz')):
-        with tarfile.open(archive_path, 'r:gz') as tar_ref:
+    elif archive_path.endswith((".tar.gz", ".tgz")):
+        with tarfile.open(archive_path, "r:gz") as tar_ref:
             tar_ref.extractall(extract_to)
     else:
         raise ValueError("Unsupported archive format")
     print(f"Extracted {archive_path} to {extract_to}")
+
 
 def calculate_sha256(file_path: str) -> str:
     sha256_hash = hashlib.sha256()
@@ -79,63 +96,79 @@ def calculate_sha256(file_path: str) -> str:
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
-def gather_files_info(repo_path: str) -> Dict[str, Union[ModulesJSON, DataJSON, ModsJSON, ClientJSON]]:
-    files_info = {
+
+def gather_files_info(
+    repo_path: str,
+) -> dict[str, ModulesJSON | DataJSON | ModsJSON | ClientJSON]:
+    files_info: dict[str, ModulesJSON | DataJSON | ModsJSON | ClientJSON] = {
         "modules": ModulesJSON(),
         "data": DataJSON(),
         "mods": ModsJSON(),
-        "client": ClientJSON()
+        "client": ClientJSON(),
     }
 
     for root, _, files in os.walk(repo_path):
         for file in files:
-            if file.startswith('.') or file.endswith('.md'):
+            if file.startswith(".") or file.endswith(".md"):
                 continue
 
             file_path = os.path.join(root, file)
             relative_path = os.path.relpath(file_path, repo_path)
             normalized_path = os.path.normpath(relative_path)
-            path_parts = normalized_path.split(os.path.sep)
-
-            if any(part.startswith('.') for part in path_parts):
-                continue
 
             file_info = FileInfo(
-                localfile=relative_path.replace(os.path.sep, '/'),
+                localfile=relative_path.replace(os.path.sep, "/"),
                 packedhash=calculate_sha256(file_path),
                 size=os.path.getsize(file_path),
-                url=relative_path.replace(os.path.sep, '/')
+                url=relative_path.replace(os.path.sep, "/"),
             )
 
-            if 'modules' in path_parts:
+            if "modules" in normalized_path:
                 files_info["modules"].files.append(file_info)
-            elif 'data' in path_parts:
+            elif "data" in normalized_path:
                 files_info["data"].files.append(file_info)
-            elif 'mods' in path_parts:
+            elif "mods" in normalized_path:
                 files_info["mods"].files.append(file_info)
-            else:
+            elif isinstance(files_info["client"], ClientJSON):
                 files_info["client"].files.append(file_info)
-                if file.endswith('.exe'):
-                    files_info["client"].executable = relative_path.replace(os.path.sep, '/')
+                if file.endswith(".exe"):
+                    files_info["client"].executable = relative_path.replace(
+                        os.path.sep, "/"
+                    )
 
     return files_info
 
-def write_json_files(files_info: Dict[str, Union[ModulesJSON, DataJSON, ModsJSON, ClientJSON]], repo_path: str) -> None:
+
+def write_json_files(
+    files_info: dict[str, ModulesJSON | DataJSON | ModsJSON | ClientJSON],
+    windows_path: str,
+) -> None:
+    os.makedirs(windows_path, exist_ok=True)
+
     for key, json_file in files_info.items():
-        json_path = os.path.join(repo_path, f"{key}.windows.json")
-        with open(json_path, 'w') as file:
+        json_path = os.path.join(windows_path, f"{key}.windows.json")
+        with open(json_path, "w") as file:
             json.dump(
                 json_file.__dict__,
                 file,
                 default=lambda o: o.__dict__ if isinstance(o, FileInfo) else o,
-                indent=4
+                indent=4,
             )
         print(f"{key}.windows.json created successfully at {json_path}")
 
+
 def main(repo: str, tag_name: str, repo_path: str) -> None:
+    windows_path = os.path.join(repo_path, "windows")
+    os.makedirs(windows_path, exist_ok=True)
+
     release_data = fetch_release_data(repo, tag_name)
     asset = get_release_asset(release_data)
-    archive_path = download_asset(asset)
-    extract_archive(archive_path, repo_path)
-    files_info = gather_files_info(repo_path)
-    write_json_files(files_info, repo_path)
+    archive_path = download_asset(asset, download_dir=windows_path)
+    extract_archive(archive_path, extract_to=windows_path)
+
+    files_info = gather_files_info(windows_path)
+    write_json_files(files_info, windows_path)
+
+    if os.path.exists(archive_path):
+        os.remove(archive_path)
+        print(f"Removed archive: {archive_path}")
